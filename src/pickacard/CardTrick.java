a3a6512f521201879b9fddb556d97470ea7cb99d
Firwanaa <firwanaa@sheridancollege.ca>
2020-06-10 15:08:45 -0400

    This is my ICE1 - 01 commit-3

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -26,7 +26,6 @@ public class CardTrick {
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             c.setSuit(Card.SUITS[c.randomSuit()]);
             magicHand[i] = c;//saving object in array
-            System.out.println( c.getSuit() + " " + c.getValue());
         }
 
         //insert code to ask the user for Card value and suit, create their card
