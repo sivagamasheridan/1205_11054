f7813011ca2087f3af685983dba34974daa540d2
Pratyancha26 <66335019+Pratyancha26@users.noreply.github.com>
2020-06-11 00:04:51 -0400

    Update Card.java

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -7,8 +7,8 @@ package pickacard;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye modifier sivagama
- * @author Paul Bonenfant
+ * @modifier Pratyancha Patil
+ * ID - 991591887
  */
 public class Card {
 
