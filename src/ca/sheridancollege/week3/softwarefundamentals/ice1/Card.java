6528e45e5569b20a72d7bba79c9a308d65592112
Ellie Asadi <65984366+EllieEAs@users.noreply.github.com>
2020-06-12 18:04:00 -0400

    Update Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -3,6 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//Ellie Asadi
+//Student Number: 991458861
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 import java.util.Random;
