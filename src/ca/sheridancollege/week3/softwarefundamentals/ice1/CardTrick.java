6b06144598b1d56dd345d70fb52c77ff0f34cbd9
Ellie Asadi <asadie@sheridancollege.ca>
2020-06-12 17:39:48 -0400

    SoftwareDesign

---
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+import java.util.Scanner;
+
+/**
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
+ *
+ * @author srinivsi
+ */
+public class CardTrick {
+
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+    
+        Card[] magicHand = new Card[7];
+
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();
+            //c.setValue(insert call to random number generator here)
+            
+            c.setValue(c.createRandomValue());
+
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(c.createRandomSuit());
+
+           
+
+             //insert code to ask the user for Card value and suit, create their card
+            System.out.println("Enter card suit");
+            String suit = input.next();
+            System.out.println("Enter card value");
+            int value = input.nextInt();
+
+            
+           
+            // and search magicHand here to find matching card is in array or not
+            //Then report the result here
+        }
+
+    }
+}
