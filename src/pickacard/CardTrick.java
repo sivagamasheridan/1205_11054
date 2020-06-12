b1dd727b3ba550d6c95be6ff81aebc03c87d89c9
user <user@LAPTOP-MO3LOTSK>
2020-06-12 17:09:08 -0400

    ice1

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,5 +1,7 @@
 package pickacard;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
@@ -17,11 +19,37 @@ public class CardTrick {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+         c.setValue(((int)Math.random()*13)+1);
+            c.setSuit(Card.SUITS[(int)Math.random()*4]);
+            magicHand[i] = c;
+        
         }
 
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+    Scanner k = new Scanner(System.in);
+        System.out.println("enter any 652 card value");
+        int value = k.nextInt();
+        System.out.println("choose suit value");
+        String suit = k.next();
+        for(int sub = 0;sub<magicHand.length;sub++)
+        {
+            if(value == magicHand[sub].getValue())
+            {
+           if(suit.equalsIgnoreCase(magicHand[sub].getSuit()))
+           {
+                 System.out.println("card found");
+           }}
+               else
+               System.out.println("card not found");  
+               
+            
+        
+        }
+            
+        
+        
     }
 
 }
