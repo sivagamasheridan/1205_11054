4c583a939ab878ff081b757e78f3f201135f8c2c
simran6511 <kaur9008@sheridancollege.ca>
2020-06-13 19:18:22 -0400

    First commit

---
+++ src/pickacard/Card.java
@@ -0,0 +1,44 @@
+
+package pickacard;
+/**
+* A class that models playing card Objects. Cards have
+* a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+* A suit (clubs, hearts, spades, diamonds).
+* There are 52 cards in a deck, no jokers.
+* This code is to be used in ICE1. When you create your own branch,
+* add your name as a modifier.
+* @author dancye modifier sivagama
+* @author Paul Bonenfant
+*/
+public class Card {
+
+   private String suits;//diamonds,clubs,spades, hearts
+    private int value; // 1 to 13
+    public static final String[] SUITS = {"Diamonds", "Clubs", "Spades", "Hearts"};
+
+    public String getSuits() {
+        return suits;
+    }
+
+    public void setSuits(String suits) {
+        this.suits = suits;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int randomValue() {
+        int value = (int) (Math.random() * 13 + 1);
+        return value;
+    }
+
+    public int randomSuit() {
+        int suit = (int) (Math.random() * 4);
+        return suit;
+    }
+}
---
+++ src/pickacard/CardTrick.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pickacard;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author parminderg876
+ */
+    public class CardTrick {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // code to generate 7 random cards and store in array
+        Card[] magicHand = new Card[7];//array of object
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();//object
+            c.setSuits(Card.SUITS[c.randomSuit()]);
+            c.setValue(c.randomValue());
+            magicHand[i] = c;//saving object in array
+            System.out.println(c.getSuits() + " " + c.getValue());
+        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Suits: ");
+        String a = input.nextLine();
+        System.out.println("Enter the value of Suits:");
+        int n = input.nextInt();
+
+        Card usercard = new Card();
+
+        usercard.setValue(n);
+        usercard.setSuits(a);
+
+        int count = 0;
+
+        for (Card magicHand1 : magicHand) {
+            if (usercard.getSuits().equalsIgnoreCase(magicHand1.getSuits()) && usercard.getValue() == magicHand1.getValue()) {
+                count = 1;
+                System.out.println("You Win!!");
+                break;
+            } else {
+                System.out.println("You Lose!!");
+                break;
+            }
+        }
+    }
+}
