4605645cdfef457f9dd7890973050d3273cca75c
wasonan <wasonan@sheridancollege.ca>
2020-06-13 01:21:20 -0400

    2nd Commit

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -12,38 +12,33 @@ package pickacard;
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+    private String suits;//diamonds,clubs,spades, hearts
+    private int value; // 1 to 13
+    public static final String[] SUITS = {"Diamonds", "Clubs", "Spades", "Hearts"};
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
-    public String getSuit() {
-        return suit;
+    public String getSuits() {
+        return suits;
     }
 
-    /**
-     * @param suit the suit to set
-     */
-    public void setSuit(String suit) {
-        this.suit = suit;
+    public void setSuits(String suits) {
+        this.suits = suits;
     }
 
-    /**
-     * @return the value
-     */
     public int getValue() {
         return value;
     }
 
-    /**
-     * @param value the value to set
-     */
     public void setValue(int value) {
         this.value = value;
     }
-   
-   
-    
+
+    public int randomValue() {
+        int value = (int) (Math.random() * 13 + 1);
+        return value;
+    }
+
+    public int randomSuit() {
+        int suit = (int) (Math.random() * 4);
+        return suit;
+    }
 }
