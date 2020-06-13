ac1176a9f1505bc48e9250429e4bce8ce83a1120
wasonan <66336124+wasonan@users.noreply.github.com>
2020-06-13 01:28:54 -0400

    Update Card.java

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -1,14 +1,8 @@
 package pickacard;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
- * @author dancye modifier sivagama
- * @author Paul Bonenfant
+ * Modifier: Ankeet Singh Wason
+ * Student ID: 991525855
  */
 public class Card {
 
