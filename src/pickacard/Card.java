5ad66f39ca077e906e09880c84854f43670efbb6
Beant15 <66038292+Beant15@users.noreply.github.com>
2020-06-13 17:30:52 -0400

    Update Card.java

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -8,7 +8,7 @@ package pickacard;
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author dancye modifier sivagama
- * @author Paul Bonenfant
+ * @author Beant Kaur
  */
 public class Card {
 
