08309d2fca43c61a56241b9e65f5fbb3f06bc515
JayPatel-1234 <66849645+JayPatel-1234@users.noreply.github.com>
2020-06-13 17:32:20 -0700

    Update CardTrick.java

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
  * @author srinivsi
- * @author Paul Bonenfant
+ * @author Jay
  */
 public class CardTrick {
 
