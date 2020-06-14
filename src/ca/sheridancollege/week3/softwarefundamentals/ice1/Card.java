7e21a841bb682bb84b12c1c019b4a2b779fc0a52
nabhag <65984754+nabhag@users.noreply.github.com>
2020-06-13 20:49:04 -0400

    Update Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Student Id: 991585410
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
