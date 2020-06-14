457dbb5afbf0b461fd3c3ab4a30b41dc3ec6ef00
nabhag <65984754+nabhag@users.noreply.github.com>
2020-06-13 20:53:53 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * NAME: Bhagesh Bhagesh
+ * Student ID: 991585410
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 import java.util.Scanner;
