ab4f4345ff97d621d087c092ea6b9b4bde0f6aea
punnja <punnja@sheridancollege.ca>
2020-06-16 16:26:02 -0400

    jaskaran branch commit

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -7,40 +7,83 @@ import java.util.Scanner;
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
  * @author srinivsi
-* @ author Jay Rajeshbhai Patel 
+* @moddifier :jaskaran singh punn
+* student id :991608479
 */
 
 public class CardTrick {
 
-    public static void main(String[] args) {
-        
-        
-        boolean position=true;
+    
+      public static void main(String[] args)
+
+    {
+
+        Scanner input=new Scanner(System.in); // input scanner
+
+       
+
         Card[] magicHand = new Card[7];
-        
-        Scanner input = new Scanner(System.in);
-        
-        for (int i = 0; i < magicHand.length; i++) {
+
+        //array list
+
+        for (int i=0; i<magicHand.length; i++)
+
+        {
+
             Card c = new Card();
+
             //c.setValue(insert call to random number generator here)
-            c.setValue(c.randNum());
+
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
 
-        
+            c.setValue((int)(Math.random()*13+1));
+
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+
+            magicHand[i]=c;
+
+            System.out.println(c.getValue()+" "+c.getSuit());
+
         }
-            if ( position == true) 
-            {
-                System.out.println("::Card is in Array::");
-            }
-            else
-            {
-                System.out.println("::Card is not in Array::");
-            }
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
 
+        System.out.println("Enter Suit");   //it display enter Suit
+
+        String s = input.next();
+
+        //user input suit
+
+        System.out.println("Enter value");   // it display enter Enter value
+
+        int value = input.nextInt();
+
+        //user input value
+
+        boolean is = true;
+
+        //return value
+
+        for (Card magicHand1 : magicHand)
+
+        {
+
+            if (value == magicHand1.getValue() && s.equalsIgnoreCase(magicHand1.getSuit()))
+
+            {
+
+                System.out.println("Your Card is in the list");
+
+                is = false;
+
+            }
+
+        }
+
+        if (is == true)
+
+        {
+
+            System.out.println("Your Card is not in the list");
+
+        }
     }
-
-    
-}
+    }
